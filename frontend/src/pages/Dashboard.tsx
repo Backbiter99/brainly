72e78fb444ba653c789f6c6b4bb4ea96604ae30a
Prajwal Jha <prajwaljha@gmail.com>
2024-12-29 14:27:25 +0530

    add responsive styling

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -4,6 +4,7 @@ import { SidebarProvider } from "../components/ui/sidebar";
 import { AppSidebar } from "../components/app-sidebar";
 import { Appcard } from "../components/AppCard";
 import { useAllContent } from "@/hooks/useAllContent";
+import { useIsMobile } from "@/hooks/use-mobile";
 
 interface contentsProps {
     _id: string;
@@ -14,6 +15,61 @@ interface contentsProps {
 
 function Dashboard() {
     const contents: contentsProps[] = useAllContent();
+
+    function checkIsMobile() {
+        const isMobile = useIsMobile();
+
+        if (isMobile) {
+            return (
+                <div className="grid grid-cols-1 w-full h-full overflow-y-scroll">
+                    <Appcard
+                        title="How to Use the Website"
+                        content="https://docs.google.com/document/d/e/2PACX-1vQd_ZWDs1RsFNYyViLAXxHTtLIHlW3_TUys0HgDt5GCIkpqIGNFZ5CP8KreL01HYY3DKLoDOT04vxOJ/pub?embedded=true"
+                        icon="Document"
+                        id="0"
+                    />
+                    {contents.length > 0 ? (
+                        contents.map(({ title, link, type, _id }, index) => (
+                            <Appcard
+                                key={index}
+                                id={_id}
+                                icon={type}
+                                title={title}
+                                content={link}
+                            />
+                        ))
+                    ) : (
+                        <p>No content available</p>
+                    )}
+                </div>
+            );
+        } else {
+            return (
+                <div className="grid grid-cols-3 w-full">
+                    <Appcard
+                        title="How to Use the Website"
+                        content="https://docs.google.com/document/d/e/2PACX-1vQd_ZWDs1RsFNYyViLAXxHTtLIHlW3_TUys0HgDt5GCIkpqIGNFZ5CP8KreL01HYY3DKLoDOT04vxOJ/pub?embedded=true"
+                        icon="Document"
+                        id="0"
+                    />
+                    {contents.length > 0 ? (
+                        contents.map(({ title, link, type, _id }, index) => (
+                            <Appcard
+                                key={index}
+                                id={_id}
+                                icon={type}
+                                title={title}
+                                content={link}
+                            />
+                        ))
+                    ) : (
+                        <p>No content available</p>
+                    )}
+                </div>
+            );
+        }
+    }
+
     return (
         <ThemeProvider>
             <SidebarProvider>
@@ -22,31 +78,7 @@ function Dashboard() {
                     <div className="my-3">
                         <Appbar />
                     </div>
-                    <main className="mt-10 w-full mx-4">
-                        <div className="grid grid-cols-3 w-full">
-                            <Appcard
-                                title="How to Use the Website"
-                                content="https://docs.google.com/document/d/e/2PACX-1vQd_ZWDs1RsFNYyViLAXxHTtLIHlW3_TUys0HgDt5GCIkpqIGNFZ5CP8KreL01HYY3DKLoDOT04vxOJ/pub?embedded=true"
-                                icon="Document"
-                                id="0"
-                            />
-                            {contents.length > 0 ? (
-                                contents.map(
-                                    ({ title, link, type, _id }, index) => (
-                                        <Appcard
-                                            key={index}
-                                            id={_id}
-                                            icon={type}
-                                            title={title}
-                                            content={link}
-                                        />
-                                    )
-                                )
-                            ) : (
-                                <p>No content available</p>
-                            )}
-                        </div>
-                    </main>
+                    <main className="mt-10 w-full mx-4">{checkIsMobile()}</main>
                 </div>
             </SidebarProvider>
         </ThemeProvider>
